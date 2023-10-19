b836e919af2ad64e7c4b49150db8c26929663960
Ferdinand Ogama <ogamagabriel@gmail.com>
2023-10-19 08:50:42 +0300

    Creating Footer

---
+++ app/Footer.tsx
@@ -0,0 +1,11 @@
+import styles from '../app/Footer.module.css'
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+          <p>Built using NextJS</p>
+    </footer>
+  );
+};
+
+export default Footer;
