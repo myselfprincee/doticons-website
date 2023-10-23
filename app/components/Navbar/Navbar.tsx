bc9c9c7a5246d10a7d982e03063932e6e33f7cc3
Prince Gupta <prince.kumar01129@gmail.com>
2023-10-23 15:42:36 +0530

    Changes to Navigation Bar

--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -1,30 +1,42 @@
-"use client"
-import styles from './Navbar.module.scss';
-import Link from 'next/link';
-import GithubButton from '../Button';
-import Image from 'next/image';
-import packageJson from '../../../package.json';
+"use client";
+import styles from "./Navbar.module.scss";
+import Link from "next/link";
+import Image from "next/image";
+import GitHubButton from "react-github-btn";
+import packageJson from "../../../package.json";
 
 export default function Navbar() {
-
-  let version = packageJson.dependencies.doticons.includes("^") ? packageJson.dependencies.doticons.replace("^", "") : packageJson.dependencies.doticons;
+  let version = packageJson.dependencies.doticons.includes("^")
+    ? packageJson.dependencies.doticons.replace("^", "")
+    : packageJson.dependencies.doticons;
   return (
-    <>
-      <nav className={styles.navbar}>
-        <div className={styles.nav_grouped}>
+    <nav className={styles.navbar}>
+      <div className={styles.nav_grouped}>
         <div className={styles.nav_left_grouped}>
-        <Link href={"/"}>
-          <Image draggable="false" width={140} height={50} src="/DotIcons.svg" alt="Doticons Logo" />
-        </Link>
-        <Link href={"https://www.npmjs.com/package/doticons"} target='_blank'>
-        <p className={styles.version}>{"v" + version}</p>
-        </Link>
+          <Link href={"/"}>
+            <Image
+              draggable="false"
+              width={140}
+              height={50}
+              src="/DotIcons.svg"
+              alt="Doticons Logo"
+            />
+          </Link>
+          <Link href={"https://www.npmjs.com/package/doticons"} target="_blank">
+            <p className={styles.version}>{"v" + version}</p>
+          </Link>
         </div>
-
-
-<GithubButton/>
-</div>
-      </nav>
-    </>
-  )
+        <GitHubButton
+          href="https://github.com/eduardconstantin/doticons-website"
+          data-color-scheme="no-preference: dark_dimmed; light: light; dark: dark_high_contrast;"
+          data-icon="octicon-star"
+          data-show-count="true"
+          aria-label="Star eduardconstantin/doticons-website on GitHub"
+        >
+          Star
+        </GitHubButton>
+      </div>
+    </nav>
+  );
 }
+
