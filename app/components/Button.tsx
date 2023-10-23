fa9fffe74f52271f9935d19c4f15818db117c406
Prince Gupta <prince.kumar01129@gmail.com>
2023-10-23 15:26:43 +0530

    Update app/components/Button.tsx
    Co-authored-by: Eduard-Constantin Ibinceanu <ibinceanu.eduard@yahoo.com>

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -3,7 +3,14 @@ import GitHubButton from 'react-github-btn'
 const GithubButton = () => {
   return (
         <>
-<GitHubButton href="https://github.com/eduardconstantin/doticons-website" data-color-scheme="no-preference: dark_dimmed; light: light; dark: dark_high_contrast;" data-icon="octicon-star" data-show-count="true" aria-label="Star eduardconstantin/doticons-website on GitHub">Star</GitHubButton>
+<GitHubButton
+          href="https://github.com/eduardconstantin/doticons"
+          data-color-scheme="no-preference: light; light: light; dark: dark;"
+          data-icon="octicon-star"
+          data-size="large"
+          data-show-count="true"
+          aria-label="Star doticons collection on GitHub"
+        >Star</GitHubButton>
         </>
   );
 };
