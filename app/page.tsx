841258f08b9543da88d2896842f0982446801c0c
Manasvi Agrawal <76566937+Manasvi1606@users.noreply.github.com>
2023-10-25 00:36:48 +0530

    added background

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import styles from './page.module.scss';
+import DotsBackground from '../components/DotsBackground';
 
 export default function Home() {
   return (
     <main className={styles.main}>
-
+      <DotsBackground />
     </main>
   );
 }
