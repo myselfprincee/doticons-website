a6b8a7d6fe71ea11e0c7dd0e7f06eb3dda9a8dd4
Prince Gupta <prince.kumar01129@gmail.com>
2023-10-25 14:18:25 +0530

    FIX: Added Responsiveness to the Dots Background

--- components/DotsBackground.js
+++ components/DotsBackground.js
@@ -1,17 +1,47 @@
+'use client';
+
 import React from 'react';
 import styles from './DotsBackground.module.css';
 
 const DotsBackground = () => {
-  const numCols = 100;
+  const numCols =
+    window.innerWidth <= 400
+      ? 10
+      : window.innerWidth <= 500
+      ? 13
+      : window.innerWidth <= 550
+      ? 14
+      : window.innerWidth <= 600
+      ? 15
+      : window.innerWidth <= 650
+      ? 16
+      : window.innerWidth <= 700
+      ? 17
+      : window.innerWidth <= 750
+      ? 18
+      : window.innerWidth <= 800
+      ? 20
+      : window.innerWidth <= 850
+      ? 21
+      : window.innerWidth <= 900
+      ? 22
+      : window.innerWidth <= 950
+      ? 23
+      : window.innerWidth <= 1000
+      ? 24
+      : 35;
+
+  const rowSpacing = window.innerWidth > 1000 ? 3 : 2;
+
   const numRows = 50;
-  const spacing = 1.095;
+  const colSpacing = 0;
 
   const circleElements = [];
 
   for (let i = 0; i < numCols; i++) {
     for (let j = 0; j < numRows; j++) {
-      const cx = i * (100 / numCols) + spacing * i + 50 / numCols;
-      const cy = j * (100 / numRows) + spacing * j + 50 / numRows;
+      const cx = i * (100 / numCols) + colSpacing * i + 50 / numCols;
+      const cy = j * (100 / numRows) + rowSpacing * j + 50 / numRows;
 
       const animationDelay = Math.random() * 1 + 's';
       const animationDuration = Math.random() * 5 + 5 + 's';
